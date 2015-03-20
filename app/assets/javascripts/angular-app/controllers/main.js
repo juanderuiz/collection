9c92f1c6d06994f21744bad5116b355cea73ca89
juanderuiz <juanderuiz@gmail.com>
2015-03-20 13:09:27 +0100

    Delete VinilosIndexCtrl and readjust in MainCtrl. Little changes in application template

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -1,12 +1,15 @@
 angular
   .module('app')
-  .controller('MainCtrl', ['Band', 'Album', '$scope', function(Band, Album, $scope){
+  .controller('MainCtrl', ['Band', 'Album', 'Vinilo', '$scope', function(Band, Album, Vinilo, $scope){
     $scope.bands = Band.query();
+    $scope.vinilos = Vinilo.all().success(function(data){
+      $scope.vinilos = data;
+    });
     
     $scope.selectAlbums = function(id){ //Get Albums from a Band
     	console.log("Buscando discos...");
     	$scope.discos = Album.get({band_id: id});
-    	$scope.totalDiscosBanda = $scope.discos.length;
+    	//$scope.totalDiscosBanda = $scope.discos.length;
     	console.log("Fin Buscando discos...");
     };
 
