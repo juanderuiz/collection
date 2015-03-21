b2cb722f4bd6f11b6ec7cb95b0d612810a3a8ff8
juanderuiz <juanderuiz@gmail.com>
2015-03-21 13:33:19 +0100

    Add single state functions for editing and creating to main.js. Also main.css

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -13,4 +13,63 @@ angular
     	console.log("Fin Buscando discos...");
     };
 
+    $scope.fullCollection = true;
+    $scope.currentBand = null;
+
+    function setFullCollection(){
+    	$scope.fullCollection = true;
+    	$scope.currentBand = null;
+
+    	cancelCreating();
+    	cancelEditing();
+    }
+
+    function setCurrentBand(band){
+    	$scope.currentBand = band;
+    	$scope.fullCollection = false;
+
+    	cancelCreating();
+    	cancelEditing();
+    }
+   
+    $scope.setFullCollection = setFullCollection;
+    $scope.setCurrentBand = setCurrentBand;
+
+    //CREATE AND EDIT
+    $scope.isCreating = false;
+    $scope.isEditing = false;
+
+    function startCreating(){
+    	$scope.isCreating = true;
+    	$scope.isEditing = false;
+    }
+
+    function cancelCreating(){
+    	$scope.isCreating = false;
+    }
+
+    function startEditing(){
+    	$scope.isEditing = true;
+    	$scope.isCreating = false;
+    }
+
+    function cancelEditing(){
+    	$scope.isEditing = false;
+    }
+
+    function shouldShowCreating(){
+    	return $scope.currentBand && !$scope.isEditing;
+    }
+
+    function shouldShowEditing(){
+    	return $scope.isEditing && !$scope.isCreating;
+    }
+
+    $scope.startCreating = startCreating;
+    $scope.startEditing = startEditing;
+    $scope.cancelCreating = cancelCreating;
+    $scope.cancelEditing = cancelEditing;
+    $scope.shouldShowCreating = shouldShowCreating;
+    $scope.shouldShowEditing = shouldShowEditing;
+
   }]);
